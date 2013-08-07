26297ae9e7764461be3c0cbe9691445a667cdc04
SeanHolden <sean.holden@o2.com>
2013-08-07 22:52:48 +0100

    Added an endpoint that users can respond to with 1, 2 or 3. This will represent yes, no and maybe.

--- server.js
+++ server.js
@@ -18,16 +18,24 @@ app.get('/', function(request, response){
   response.end();
 });
 
-app.post('/yes', function(request, response){
-  response.end();
-});
+app.post('/response', function(request, response){
+  console.log("Body = " + request.body.Body);
+  console.log("From = " + request.body.From);
 
-app.post('/no', function(request, response){
-  response.end();
-});
+  var body = request.body.Body;
+  var from = request.body.From;
 
-app.post('/maybe', function(request, response){
-  response.end();
+  if( body.trim()[0] == '1' ){
+    console.log('Yep, %s will be in tomorrow', from);
+  }else if(body.trim()[0] == '2' ){
+    console.log('Nope, %s will not be in tomorrow', from);
+  }else if(body.trim()[0] == '3' ){
+    console.log('Maybe %s will be in tomorrow', from);
+  }else{
+    console.log('Unrecognised response. Not 1, 2 or 3.');
+  };
+
+  response.end("Thanks, message received!");
 });
 
 var port = process.env.PORT || 3000;
